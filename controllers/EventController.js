abd656c0fb1ed37034284182de96a0470ce2257f
Felipe Fluck <felipe@fluck.com.ar>
2023-06-18 18:45:47 -0300

    feat: join band to event (body usage, frontend put with bandId)

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -1,5 +1,5 @@
-import BarService from "../services/BarService.js";
-import EventService from "../services/EventService.js";
+import BarService from '../services/BarService.js';
+import EventService from '../services/EventService.js';
 
 export default class EventController {
   constructor() {
@@ -14,27 +14,27 @@ export default class EventController {
       const events = await this.eventService.getEvents();
       return res.json(events);
     } catch (error) {
-      console.error("Error getting events with get", error);
+      console.error('Error getting events with get', error);
     }
   };
 
   getEventById = async (req, res) => {
     const { id } = req.params;
     if (!id) {
-      return res.status(400).send("Empty id");
+      return res.status(400).send('Empty id');
     }
     try {
       const event = await this.eventService.getEventById(id);
       return res.json(event);
     } catch (error) {
-      console.error("Error getting event by id get", error);
-      returnres.status(500).send("Check id");
+      console.error('Error getting event by id get', error);
+      returnres.status(500).send('Check id');
     }
   };
 
   createEvent = async (req, res) => {
     if (Object.keys(req.body).length <= 0) {
-      return res.status(400).send("Empty event object");
+      return res.status(400).send('Empty event object');
     }
     try {
       const { event, username } = req.body;
@@ -47,36 +47,36 @@ export default class EventController {
 
       return res.json(newEvent);
     } catch (error) {
-      console.error("Error creating event with post", error);
-      res.status(500).send("Check your fields");
+      console.error('Error creating event with post', error);
+      res.status(500).send('Check your fields');
     }
   };
 
   updateEvent = async (req, res) => {
     if (Object.keys(req.body).length <= 0) {
-      return res.status(400).send("Empty object");
+      return res.status(400).send('Empty object');
     }
     try {
       const { id } = req.params;
       const updatedEvent = await this.eventService.updateEvent(id, req.body);
       return res.json(updatedEvent);
     } catch (error) {
-      console.log("Error updating event with put ", error);
-      return res.status(500).send("Check your fields");
+      console.log('Error updating event with put ', error);
+      return res.status(500).send('Check your fields');
     }
   };
 
   deleteEvent = async (req, res) => {
     const { id } = req.params;
     if (!id) {
-      return res.status(400).send("Id not found");
+      return res.status(400).send('Id not found');
     }
     try {
       const deletedEvent = await this.eventService.deleteEvent(id);
       return res.json(deletedEvent);
     } catch (error) {
-      console.log("Error deleting event: ", error);
-      return res.status(500).send("Check the id");
+      console.log('Error deleting event: ', error);
+      return res.status(500).send('Check the id');
     }
   };
 }
