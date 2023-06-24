ec415182a8108e95cbb768a462dad8f25fd131ac
Pedro Kupersmit <pkupersmit@gmail.com>
2023-06-24 17:55:17 -0300

    fix: change export from validations.js

--- model/mongoDbValidations/validations.js
+++ model/mongoDbValidations/validations.js
@@ -1,32 +1,33 @@
 /* CAPACITY */
-function validateCapacity(value) {
+export function validateCapacity(value) {
   return value > 0 && value < 1000000;
 }
 
-const capacityError = "Los límites de capacidad son incorrectos";
+export const capacityError = "Los límites de capacidad son incorrectos";
 
 /* NAME */
-const nameError = "El nombre debe tener al menos 3 caracteres";
+export const nameError = "El nombre debe tener al menos 3 caracteres";
 
 /* ADDRESS */
-const addressError = "La dirección debe tener al menos 3 caracteres";
+export const addressError = "La dirección debe tener al menos 3 caracteres";
 
 /* PASSWORD */
-const passwordError = "La contraseña debe tener al menos 3 caracteres";
+export const passwordError = "La contraseña debe tener al menos 3 caracteres";
 
 /* PRICE */
-function validatePrice(value) {
+export function validatePrice(value) {
   return value > 0 && value < 10000000;
 }
 
-const priceError = "Los límites de capacidad son incorrectos";
+export const priceError = "Los límites de capacidad son incorrectos";
 
-export default {
-  validateCapacity,
-  capacityError,
-  nameError,
-  addressError,
-  passwordError,
-  validatePrice,
-  priceError,
-};
+// export default  {
+//   validateCapacity,
+//   capacityError,
+//   nameError,
+//   addressError,
+//   passwordError,
+//   validatePrice,
+//   priceError,
+// };
+
