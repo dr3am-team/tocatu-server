126148dcd829b2b3a13d77639289a849b172f10b
Felipe Fluck <felipe@fluck.com.ar>
2023-06-26 19:50:42 -0300

    fix: deleted ext test

--- test/api.int.test.js
+++ test/api.int.test.js
@@ -1,18 +1,18 @@
-import { expect } from "chai";
-import supertest from "supertest";
-import generador from "./generador/producto.js";
+import { expect } from 'chai';
+import supertest from 'supertest';
+import generador from './generador/producto.js';
 
-import Server from "../server.js";
+import Server from '../server.js';
 
 //This test suit needs to have the server off
-describe("Test API REST ful: Test Interno", () => {
-  describe("GET", () => {
-    it("Debería retornar un status 200", async () => {
+describe('Test API REST ful: Test Interno', () => {
+  describe('GET', () => {
+    it('Debería retornar un status 200', async () => {
       const server = new Server();
       const app = await server.start();
 
       const request = supertest(app);
-      const response = await request.get("/api/users");
+      const response = await request.get('/api/bars');
 
       expect(response.status).to.eql(200);
 
@@ -20,8 +20,8 @@ describe("Test API REST ful: Test Interno", () => {
     });
   });
 
-  describe("POST", () => {
-    it("Debería agregar un usuario", async () => {
+  describe('POST', () => {
+    it('Debería agregar un usuario', async () => {
       const server = new Server();
       const app = await server.start();
 
@@ -30,11 +30,11 @@ describe("Test API REST ful: Test Interno", () => {
       const user = generador.get();
       console.log(user);
 
-      const response = await request.post("/api/users").send(user);
+      const response = await request.post('/api/users').send(user);
       expect(response.status).to.eql(200);
 
       const userResponse = response.body;
-      expect(userResponse).to.include.keys("username", "password", "mail");
+      expect(userResponse).to.include.keys('username', 'password', 'mail');
 
       expect(userResponse.username).to.eql(user.username);
       expect(userResponse.password).to.eql(user.password);
