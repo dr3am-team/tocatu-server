53e8aa8e135524fde584b16d6a88142239f9434a
Pedro Kupersmit <pkupersmit@gmail.com>
2023-06-27 13:22:27 -0300

    :broom:

--- routes/PublicRoute.js
+++ routes/PublicRoute.js
@@ -4,7 +4,6 @@ import path from "path";
 const Router = express.Router();
 
 export default Router.get("/:id", (req, res) => {
-  console.log(req.params.id);
   const imagePath = path.resolve(`public/uploads/${req.params.id}`);
   res.sendFile(imagePath);
 });
